13a32522f18e6a71d9f4d7ebfc1283bf0c350c0f
Daniel Vitor Pereira da Silva <danielvitor081@gmail.com>
2021-05-13 19:26:59 -0300

    1

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+var img = document.querySelector("#image");
+
+img.setAttribute('src', 'images/hero-mobile.jpg');
+
+/*
+function sizeOfThings(){
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+    
+    var screenWidth = screen.width;
+    var screenHeight = screen.height;
+    
+    document.querySelector('.window-size').innerHTML = windowWidth + 'x' + windowHeight;
+    document.querySelector('.screen-size').innerHTML = screenWidth + 'x' + screenHeight;
+  
+  };
+  
+  sizeOfThings();
+  
+  window.addEventListener('resize', function(){
+      sizeOfThings();
+  });*/
