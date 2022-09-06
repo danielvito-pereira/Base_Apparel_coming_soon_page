9c4f08f56213478cf0ee9fd863a94e8f49f3237e
Daniel Vitor Pereira da Silva <64044840+danielvito-pereira@users.noreply.github.com>
2022-09-06 18:08:02 -0300

    teste

--- scripts/script.js
+++ scripts/script.js
@@ -12,41 +12,37 @@ window.onload = function()
 
   if(largura>=1024)
   {
-    img.setAttribute('src', 'images/hero-desktop.jpg');
+    img.setAttribute('src', "./images/hero-desktop.jpg");
   
   } else {
-    img.setAttribute('src', 'images/hero-mobile.jpg');
+    img.setAttribute('src', "/images/hero-mobile.jpg");
    
   }
 } // fim do window.onload
 
 
-function validacaoEmail(field) {
-  usuario = field.value.substring(0, field.value.indexOf("@"));
-  dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
-  var error = document.getElementById('campo_busca');
 
-  if ((usuario.length >=1)&&
-      (dominio.length >= 3) &&
-      (usuario.search("@")==-1) &&
-      (dominio.search("@")==-1) &&
-      (usuario.search(" ")==-1) &&
-      (dominio.search(" ")==-1) &&
-      (dominio.search(".")!=-1) &&
-      (dominio.indexOf(".") >=1)&&
-    (dominio.lastIndexOf(".") < dominio.length - 1)) {
-      document.getElementById("campo_busca").innerHTML="E-mail válido";
-      alert("E-mail valido");
-    } else {
-      document.getElementById('campo_busca').innerHTML="E-mail inválido";
-      alert("E-mail inválido");
-    }
-}
 
-function showNavOnScroll() {
-  if (scrollY > 0) {
-    navigation.classList.add("scroll");
-  } else {
-    navigation.classList.remove("scroll");
+
+const enviar = document.getElementById("enviar");
+
+enviar.addEventListener('click', validate);
+
+function validate(e) {
+  e.preventDefault();
+
+  const campo_busca = document.getElementById('campo_busca');
+  let valid = true;
+
+  if (!campo_busca.value) {
+    const emailError = document.getElementById('emailError');
+    emailError.classList.add("visible");
+    enviar.classList.add("invalid");
+    emailError.setAttribute('aria-hidden', false);
+    emailError.setAttribute('aria-invalid', true);
   }
+  return valid;
+
 }
+
+
